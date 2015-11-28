81bf9b40f6d4ea8ebcd4e2ea2f562eb8a7d233b4
Simon <markhams@tcd.ie>
2015-11-28 14:10:08 +0000

    Added Metrics

--- main.cpp
+++ main.cpp
@@ -420,15 +420,36 @@ void templateMatchImages(Mat * full_image,int sizeB, int sizeT, Mat * templates,
 // True Negative = no page visible and no page identified
 // False Negative = a page is visible but no page was found
 void compareAgainstGroundTruth( Mat * images, Mat * templates, int * results, int size, Mat * result){
-	int count = 0;
-
-	//for(int i = 0; i < size; i++) cout << results[i]<<endl;
+	int TP = 0, FP = 0;
+	int TN = 0, FN = 0;
 	for(int i = 0; i < size; i++){
-		if(results[i] == groundTruths[i]) count++;
-		else cout <<"Did not match: Matched Page "<<results[i]<<" and Ground Truth "<<groundTruths[i]<<endl;
+		if(results[i] == groundTruths[i]) TP++;
+		else {
+			cout <<"Did not match: Matched Page "<<results[i]<<" and Ground Truth "<<groundTruths[i]<<endl;
+			FP++;
+		}
 		result[i] = templates[results[i]];
 	}
-	cout <<"Matched a total of: "<< count<<endl;
+
+	double precision = ((double) TP) / ((double) (TP+FP));
+	double recall = ((double) TP) / ((double) (TP+FN));
+	double accuracy = ((double) (TP+TN)) / ((double) (TP+FP+TN+FN));
+	double f1 = 2.0* ((precision*recall) / (precision + recall));
+	double specificity = ((double) TN) / ((double) (FP+TN));
+
+	cout<<"---------------------------------------"<<endl;
+	cout <<"True Positives: " << TP <<endl;
+	cout <<"False Positives: " << FP << endl;
+	cout <<"True Negatives: " << TN <<endl;
+	cout <<"False Negatives: " << FN << endl;
+	cout<<"---------------------------------------"<<endl;
+	cout<<"Precision: "<<precision<<endl;
+	cout<<"Recall: "<<recall<<endl;
+	cout<<"Accuracy: "<<accuracy<<endl;
+	cout<<"Specificity: "<<specificity<<endl;
+	cout<<"F1 Measure: "<<f1<<endl;
+	cout<<"---------------------------------------"<<endl;
+
 }
 // Function to run program
 int main(int argc, const char** argv){
